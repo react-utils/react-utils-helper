7e018c0968e8c85f7e0f29edb6adebdaf8e18ef1
Sahusoft <suraj@sahusoft.com>
2015-03-03 13:42:56 +0530

    Gulpfile Added

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,16 +6,17 @@ grunt.loadNpmTasks('grunt-es6-transpiler');
 grunt.initConfig({
   react: {
     dynamic_mappings: {
-
-        options:{
+        options: {
           harmony: true
-        }, 
-        expand: true,
-        cwd: 'src/components/ComponentHelper/',
-        src: ['**/*.jsx'],
-        dest: 'lib',
-        ext: '.js'
-    },
+        },
+        
+          expand: true,
+          cwd: 'src/components/ComponentHelper/',
+          src: ['**/*.jsx'],
+          dest: 'lib',
+          ext: '.js'
+        
+    }
 
   }
 
---
+++ Gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var react = require('gulp-react');
+ 
+gulp.task('default', function () {
+    return gulp.src('src/components/ComponentHelper/**/*.jsx')
+        .pipe(react({harmony: true}))
+        .pipe(gulp.dest('lib'));
+});
