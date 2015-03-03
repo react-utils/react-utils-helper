d0bba270d1d256be80485698e94e578e0347054a
Sahusoft <suraj@sahusoft.com>
2015-03-03 16:02:41 +0530

    Modified gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var react = require('gulp-react');
  
 gulp.task('default', function () {
-    return gulp.src('src/components/ComponentHelper/**/*.jsx')
+    return gulp.src(['src/components/ComponentHelper/**/*.jsx','src/mixins/*.js'])
         .pipe(react({harmony:true}))
         .pipe(gulp.dest('lib'));
 });
