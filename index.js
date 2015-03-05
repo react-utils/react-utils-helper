cc37295fcc8ffa1c8585614d97bbb5f3502ff745
Sahusoft <suraj@sahusoft.com>
2015-03-05 18:13:36 +0530

    Minor Fixes

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * @jsx React.DOM
+ */
+
+'use strict';
+
+module.exports = {
+	Hide: require('./lib/hide'),
+	Show: require('./lib/show'),
+	Switch: require('./lib/switch'),
+	When: require('./lib/switch/when'),
+	Default: require('./lib/switch/default'),	
+	Input: require('./lib/input'),
+	Repeat: require('./lib/repeat'),
+	Textarea: require('./lib/textarea'),
+	Select: require('./lib/select'),
+	Checkbox: require('./lib/checkbox'),
+	Radio: require('./lib/Radio'),
+	FormMixin: require('./lib/formMixin')
+};
