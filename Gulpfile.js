271faec03348c60870f0792f54548a69095f9f38
Sahusoft <suraj@sahusoft.com>
2015-03-05 18:50:20 +0530

    Minor Changes

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,7 @@ var react = require('gulp-react');
 gulp.task('default', function () {
     return gulp.src([
     		'src/components/ComponentHelper/**/*.jsx',
+    		'src/components/ComponentHelper/index.js',
     		'src/mixins/formMixin.js'])
         .pipe(react({harmony:true}))
         .pipe(gulp.dest('lib'));
