0b6d4878d1c4fdc1fdd7a214d76028f1d948e943
Sahusoft <suraj@sahusoft.com>
2015-03-09 12:41:21 +0530

    Minor Changes

--- examples/Switch.js
+++ examples/Switch.js
@@ -2,13 +2,13 @@ var value =	2;
 
 var TestComponent = (
     <RU.Switch expression={value}>
-			<RU.When switchCase={1}>one</RU.When>
-			<RU.When switchCase={2}>two</RU.When>
-			<RU.When switchCase={3}>three</RU.When>
-			<RU.When switchCase={4}>four</RU.When>
-			<RU.When switchCase={5}>five</RU.When>
-			<RU.When switchCase={6}>six</RU.When>
-			<RU.Default>default</RU.Default>
+			<RU.When switchCase={1}>One</RU.When>
+			<RU.When switchCase={2}>Two</RU.When>
+			<RU.When switchCase={3}>Three</RU.When>
+			<RU.When switchCase={4}>Four</RU.When>
+			<RU.When switchCase={5}>Five</RU.When>
+			<RU.When switchCase={6}>Six</RU.When>
+			<RU.Default>Default</RU.Default>
 	</RU.Switch>
   );
 
